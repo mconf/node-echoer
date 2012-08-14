267795a5ad427e438b6e53f2cf82174a386978f0
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-08-14 17:14:33 -0300

    Add "port" option

--- app.coffee
+++ app.coffee
@@ -1,6 +1,18 @@
 http = require 'http'
 
-server = module.exports = http.createServer (req, res) ->
+app = {}
+app.config = {}
+app.config.port = 3000
+
+parser = require('commander');
+parser
+  .option("-p, --port [value]", "Port to listen to [3000]", app.config.port)
+  .parse(process.argv);
+app.config.port = parser.port if (parser.port)
+
+app.server = http.createServer (req, res) ->
   console.log new Date().toTimeString(), ":", req.url
   res.writeHead(200, {'Content-Type': 'text/plain'})
   res.end()
+
+module.exports = app
--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // This is a simple wrapper to run the app with 'node app.js'
 require("coffee-script");
-var port = 3000;
 
 // Start the server
 app = require('./app.coffee');
-app.listen(port);
-if (app.address() === null) {
-  console.log('Could not bind to port', config.lb.port);
+app.server.listen(app.config.port);
+if (app.server.address() === null) {
+  console.log('Could not bind to port', app.config.port);
   console.log('Aborting.');
   process.exit(1);
 }
-console.log("*** Server listening on port", app.address().port);
+console.log("*** Server listening on port", app.server.address().port);
